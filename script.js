a0a59ddae2eb8f3557ed1aad1e2c134e210edd15
min-uuu <minuuu03@gmail.com>
2020-08-31 12:10:02 +0900

    자동으로 연력되는 시간을 5분 뒤로 수정하였습니다

--- script.js
+++ script.js
@@ -1,44 +1,26 @@
 const links = {
-  "진로": "zoommtg://zoom.us/join?action=join&confno=8448373394&pwd=dimigo",
-  "자료구조 B": "zoommtg://zoom.us/join?action=join&confno=4679526373&pwd=py2020",
-  "음악": "https://bit.ly/34rBBfM",
   "국어": "zoommtg://zoom.us/join?action=join&confno=3300965795&pwd=dimigo",
-  "사회": "https://bit.ly/34rBBfM",
-  "수학 - 류명희": "zoommtg://zoom.us/join?action=join&confno=6838770867&pwd=dimigo",
-  "과학": "zoommtg://zoom.us/join?action=join&confno=7178015132&pwd=dimigo",
+  "국어 - 김태철": "https://us02web.zoom.us/j/8134411742?pwd=2P5EGorAeZ3uZTEN4UsZTO6utKTGGQ",
   "영어 - 공정도": "zoommtg://zoom.us/join?action=join&confno=4700404365&pwd=dimigo",
   "영어 - 전유원": "https://zoom.us/j/7385237525?pwd=dHY5SVBuVUNrL0psZjVJU3VKR3JZQT09",
-  "컴일": "zoommtg://zoom.us/join?action=join&confno=6971690768&pwd=dimigo",
-  "CA": "",
-  "체육": "zoommtg://zoom.us/join?action=join&confno=7760965125&pwd=dimigo",
-  "자료구조 A": "zoommtg://zoom.us/join?action=join&confno=433%202940405&pwd=dimigo",
-  "HR": "",
-  "미술": "zoommtg://zoom.us/join?action=join&confno=8448373394&pwd=dimigo",
+  "수학 - 류명희": "zoommtg://zoom.us/join?action=join&confno=6838770867&pwd=dimigo",
   "수학 - 이효현": "https://zoom.us/j/7987833794",
+  "사회": "https://bit.ly/34rBBfM",
+  "과학": "zoommtg://zoom.us/join?action=join&confno=7178015132&pwd=dimigo",
   "역사": "https://us04web.zoom.us/j/3955689232?pwd=eUNCRXoxcDgxKzdhZkJZdzdQTjFmdz09",
   "상경": "https://zoom.us/j/3154650453",
   "컴그": "https://zoom.us/j/7987737883",
-  "국어 - 김태철": "https://us02web.zoom.us/j/8134411742?pwd=2P5EGorAeZ3uZTEN4UsZTO6utKTGGQ"
+  "컴일": "zoommtg://zoom.us/join?action=join&confno=6971690768&pwd=dimigo",
+  "자료구조 A": "zoommtg://zoom.us/join?action=join&confno=433%202940405&pwd=dimigo",
+  "자료구조 B": "zoommtg://zoom.us/join?action=join&confno=4679526373&pwd=py2020",
+  "음악": "https://bit.ly/34rBBfM",
+  "체육": "zoommtg://zoom.us/join?action=join&confno=7760965125&pwd=dimigo",
+  "진로": "zoommtg://zoom.us/join?action=join&confno=8448373394&pwd=dimigo",
+  "미술": "zoommtg://zoom.us/join?action=join&confno=8448373394&pwd=dimigo",
+  "CA": "",
+  "HR": "",
 };
 const schedule = {
-  "1-3": {
-    "Mon": ['영어 - 공정도', '자료구조 A', '자료구조 A', '음악', '자료구조 B', '자료구조 B', '수학 - 류명희'],
-    "Tue": ['수학 - 류명희', '과학', '체육', '음악', '컴일', '진로', 'CA'],
-    "Wed": ['자료구조 A', '영어 - 공정도', '진로', '국어', '사회', '수학 - 류명희'],
-    "Thu": ['컴일', '과학', '사회', '국어', '음악', '체육', 'HR'],
-    "Fri": ['국어', '사회', '자료구조 B', '컴일', '자료구조 A', '과학', '영어 - 공정도'],
-    "Sat": ['No Schedule'],
-    "Sun": ['No Schedule']
-  },
-  "1-4": {
-    "Mon": ['진로', '자료구조 B', '음악', '국어', '사회', '수학 - 류명희', '과학'],
-    "Tue": ['자료구조 B', '자료구조 B', '영어 - 공정도', '컴일', '국어', 'CA', 'CA'],
-    "Wed": ['수학 - 류명희', '체육', '자료구조 A', '사회', '영어 - 공정도', '과학', ''],
-    "Thu": ['음악', '국어', '과학', '컴일', '자료구조 A', '자료구조 A', 'HR'],
-    "Fri": ['사회', '컴일', '음악', '영어 - 공정도', '체육', '수학 - 류명희', '자료구조 A'],
-    "Sat": ['No Schedule'],
-    "Sun": ['No Schedule']
-  },
   "1-1": {
     "Mon": ['사회', '음악', '영어 - 공정도', '역사', '컴그', '컴그', '수학 - 이효현'],
     "Tue": ['상경', '사회', '국어 - 김태철', '과학', '체육', 'CA', 'CA'],
@@ -54,6 +36,24 @@ const schedule = {
     "Wed": ['과학', '국어', '체육', '상경', '컴그', '컴그', ''],
     "Thu": ['사회', '상경', '영어 - 공정도', '역사', '수학 - 류명희', '미술', 'HR'],
     "Fri": ['수학 - 류명희', '과학', '국어 - 김태철', '진로', '영어 - 공정도', '체육', '역사'],
+    "Sat": ['No Schedule'],
+    "Sun": ['No Schedule']
+  },
+  "1-3": {
+    "Mon": ['영어 - 공정도', '자료구조 A', '자료구조 A', '음악', '자료구조 B', '자료구조 B', '수학 - 류명희'],
+    "Tue": ['수학 - 류명희', '과학', '체육', '음악', '컴일', '진로', 'CA'],
+    "Wed": ['자료구조 A', '영어 - 공정도', '진로', '국어', '사회', '수학 - 류명희'],
+    "Thu": ['컴일', '과학', '사회', '국어', '음악', '체육', 'HR'],
+    "Fri": ['국어', '사회', '자료구조 B', '컴일', '자료구조 A', '과학', '영어 - 공정도'],
+    "Sat": ['No Schedule'],
+    "Sun": ['No Schedule']
+  },
+  "1-4": {
+    "Mon": ['진로', '자료구조 B', '음악', '국어', '사회', '수학 - 류명희', '과학'],
+    "Tue": ['자료구조 B', '자료구조 B', '영어 - 공정도', '컴일', '국어', 'CA', 'CA'],
+    "Wed": ['수학 - 류명희', '체육', '자료구조 A', '사회', '영어 - 공정도', '과학', ''],
+    "Thu": ['음악', '국어', '과학', '컴일', '자료구조 A', '자료구조 A', 'HR'],
+    "Fri": ['사회', '컴일', '음악', '영어 - 공정도', '체육', '수학 - 류명희', '자료구조 A'],
     "Sat": ['No Schedule'],
     "Sun": ['No Schedule']
   },
@@ -149,7 +149,7 @@ const loop = () => {
     elements[0].classList.add('selected');
     updateH2(todaySchedule[0], links[todaySchedule[0]], true);
     redirect(links[todaySchedule[0]], todaySchedule[0]);
-  } else if (hour <= 9 && min < 50) {
+  } else if (hour <= 9 && min < 55) {
     elements[0].classList.add('selected');
     updateH2(todaySchedule[0], links[todaySchedule[0]], false);
     redirect(links[todaySchedule[0]], todaySchedule[0]);
@@ -157,7 +157,7 @@ const loop = () => {
     elements[1].classList.add('selected');
     updateH2(todaySchedule[1], links[todaySchedule[1]], true);
     redirect(links[todaySchedule[1]], todaySchedule[1]);
-  } else if (hour <= 10 && min < 50) {
+  } else if (hour <= 10 && min < 55) {
     elements[1].classList.add('selected');
     updateH2(todaySchedule[1], links[todaySchedule[1]], false);
     redirect(links[todaySchedule[1]], todaySchedule[1]);
@@ -165,7 +165,7 @@ const loop = () => {
     elements[2].classList.add('selected');
     updateH2(todaySchedule[2], links[todaySchedule[2]], true);
     redirect(links[todaySchedule[2]], todaySchedule[2]);
-  } else if (hour <= 11 && min < 50) {
+  } else if (hour <= 11 && min < 55) {
     elements[2].classList.add('selected');
     updateH2(todaySchedule[2], links[todaySchedule[2]], false);
     redirect(links[todaySchedule[2]], todaySchedule[2]);
@@ -173,7 +173,7 @@ const loop = () => {
     elements[3].classList.add('selected');
     updateH2(todaySchedule[3], links[todaySchedule[3]], true);
     redirect(links[todaySchedule[3]], todaySchedule[3]);
-  } else if (hour <= 12 && min < 50) {
+  } else if (hour <= 12 && min < 55) {
     elements[3].classList.add('selected');
     updateH2(todaySchedule[3], links[todaySchedule[3]], false);
     redirect(links[todaySchedule[3]], todaySchedule[3]);
@@ -183,7 +183,7 @@ const loop = () => {
     elements[4].classList.add('selected');
     updateH2(todaySchedule[4], links[todaySchedule[4]], true);
     redirect(links[todaySchedule[4]], todaySchedule[4]);
-  } else if (hour == 13 || (hour <= 14 && min < 40)) {
+  } else if (hour == 13 || (hour <= 14 && min < 45)) {
     elements[4].classList.add('selected');
     updateH2(todaySchedule[4], links[todaySchedule[4]], false);
     redirect(links[todaySchedule[4]], todaySchedule[4]);
@@ -191,7 +191,7 @@ const loop = () => {
     elements[5].classList.add('selected');
     updateH2(todaySchedule[5], links[todaySchedule[5]], true);
     redirect(links[todaySchedule[5]], todaySchedule[5]);
-  } else if (hour == 14 || (hour <= 15 && min < 40)) {
+  } else if (hour == 14 || (hour <= 15 && min < 45)) {
     elements[5].classList.add('selected');
     updateH2(todaySchedule[5], links[todaySchedule[5]], false);
     redirect(links[todaySchedule[5]], todaySchedule[5]);
@@ -199,7 +199,7 @@ const loop = () => {
     elements[6].classList.add('selected');
     updateH2(todaySchedule[6], links[todaySchedule[6]], true);
     redirect(links[todaySchedule[6]], todaySchedule[6]);
-  } else if (hour == 15 || (hour <= 16 && min < 40)) {
+  } else if (hour == 15 || (hour <= 16 && min < 45)) {
     elements[6].classList.add('selected');
     updateH2(todaySchedule[6], links[todaySchedule[6]], false);
     redirect(links[todaySchedule[6]], todaySchedule[6]);
