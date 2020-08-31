219b82493a25f8cde435c97fadce79745037444a
HyeokjinKang <coupy@croissant.tech>
2020-08-31 02:59:40 +0000

    1학년 전체 반 대응 및 일부 표기 변경

--- script.js
+++ script.js
@@ -2,76 +2,76 @@ const links = {
   "진로": "zoommtg://zoom.us/join?action=join&confno=8448373394&pwd=dimigo",
   "자료구조 B": "zoommtg://zoom.us/join?action=join&confno=4679526373&pwd=py2020",
   "음악": "https://bit.ly/34rBBfM",
-  "국어": "zoommtg://zoom.us/join?action=join&confno=3300965795&pwd=dimigo",
+  "국어(성)": "zoommtg://zoom.us/join?action=join&confno=3300965795&pwd=dimigo",
   "사회": "https://bit.ly/34rBBfM",
-  "수학 - 류명희": "zoommtg://zoom.us/join?action=join&confno=6838770867&pwd=dimigo",
+  "수학(류)": "zoommtg://zoom.us/join?action=join&confno=6838770867&pwd=dimigo",
   "과학": "zoommtg://zoom.us/join?action=join&confno=7178015132&pwd=dimigo",
-  "영어 - 공정도": "zoommtg://zoom.us/join?action=join&confno=4700404365&pwd=dimigo",
-  "영어 - 전유원": "https://zoom.us/j/7385237525?pwd=dHY5SVBuVUNrL0psZjVJU3VKR3JZQT09",
+  "영어(공)": "zoommtg://zoom.us/join?action=join&confno=4700404365&pwd=dimigo",
+  "영어(전)": "https://zoom.us/j/7385237525?pwd=dHY5SVBuVUNrL0psZjVJU3VKR3JZQT09",
   "컴일": "zoommtg://zoom.us/join?action=join&confno=6971690768&pwd=dimigo",
   "CA": "",
   "체육": "zoommtg://zoom.us/join?action=join&confno=7760965125&pwd=dimigo",
   "자료구조 A": "zoommtg://zoom.us/join?action=join&confno=433%202940405&pwd=dimigo",
   "HR": "",
   "미술": "zoommtg://zoom.us/join?action=join&confno=8448373394&pwd=dimigo",
-  "수학 - 이효현": "https://zoom.us/j/7987833794",
+  "수학(이)": "https://zoom.us/j/7987833794",
   "역사": "https://us04web.zoom.us/j/3955689232?pwd=eUNCRXoxcDgxKzdhZkJZdzdQTjFmdz09",
   "상경": "https://zoom.us/j/3154650453",
   "컴그": "https://zoom.us/j/7987737883",
-  "국어 - 김태철": "https://us02web.zoom.us/j/8134411742?pwd=2P5EGorAeZ3uZTEN4UsZTO6utKTGGQ"
+  "국어(김)": "https://us02web.zoom.us/j/8134411742?pwd=2P5EGorAeZ3uZTEN4UsZTO6utKTGGQ"
 };
 const schedule = {
-  "1-3": {
-    "Mon": ['영어 - 공정도', '자료구조 A', '자료구조 A', '음악', '자료구조 B', '자료구조 B', '수학 - 류명희'],
-    "Tue": ['수학 - 류명희', '과학', '체육', '음악', '컴일', '진로', 'CA'],
-    "Wed": ['자료구조 A', '영어 - 공정도', '진로', '국어', '사회', '수학 - 류명희'],
-    "Thu": ['컴일', '과학', '사회', '국어', '음악', '체육', 'HR'],
-    "Fri": ['국어', '사회', '자료구조 B', '컴일', '자료구조 A', '과학', '영어 - 공정도'],
-    "Sat": ['No Schedule'],
-    "Sun": ['No Schedule']
-  },
-  "1-4": {
-    "Mon": ['진로', '자료구조 B', '음악', '국어', '사회', '수학 - 류명희', '과학'],
-    "Tue": ['자료구조 B', '자료구조 B', '영어 - 공정도', '컴일', '국어', 'CA', 'CA'],
-    "Wed": ['수학 - 류명희', '체육', '자료구조 A', '사회', '영어 - 공정도', '과학', ''],
-    "Thu": ['음악', '국어', '과학', '컴일', '자료구조 A', '자료구조 A', 'HR'],
-    "Fri": ['사회', '컴일', '음악', '영어 - 공정도', '체육', '수학 - 류명희', '자료구조 A'],
-    "Sat": ['No Schedule'],
-    "Sun": ['No Schedule']
-  },
   "1-1": {
-    "Mon": ['사회', '음악', '영어 - 공정도', '역사', '컴그', '컴그', '수학 - 이효현'],
-    "Tue": ['상경', '사회', '국어 - 김태철', '과학', '체육', 'CA', 'CA'],
-    "Wed": ['영어 - 공정도', '상경', '음악', '역사', '과학', '수학 - 이효현', ''],
-    "Thu": ['영어 - 공정도', '상경', '음악', '역사', '과학', '국어', 'HR'],
-    "Fri": ['수학 - 이효현', '영어 - 공정도', '역사', '과학', '국어', '음악', '진로'],
+    "Mon": ['사회', '음악', '영어(공)', '역사', '컴그', '컴그', '수학(이)'],
+    "Tue": ['상경', '사회', '국어(김)', '과학', '체육', 'CA', 'CA'],
+    "Wed": ['영어(공)', '상경', '음악', '역사', '과학', '수학(이)', ''],
+    "Thu": ['영어(공)', '상경', '음악', '역사', '과학', '국어(성)', 'HR'],
+    "Fri": ['수학(이)', '영어(공)', '역사', '과학', '국어(성)', '음악', '진로'],
     "Sat": ['No Schedule'],
     "Sun": ['No Schedule']
   },
   "1-2": {
-    "Mon": ['역사', '사회', '수학 - 류명희', '영어 - 공정도', '미술', '미술', '상경'],
-    "Tue": ['국어', '컴그', '컴그', '사회', '과학', 'CA', 'CA'],
-    "Wed": ['과학', '국어', '체육', '상경', '컴그', '컴그', ''],
-    "Thu": ['사회', '상경', '영어 - 공정도', '역사', '수학 - 류명희', '미술', 'HR'],
-    "Fri": ['수학 - 류명희', '과학', '국어 - 김태철', '진로', '영어 - 공정도', '체육', '역사'],
+    "Mon": ['역사', '사회', '수학(류)', '영어(공)', '미술', '미술', '상경'],
+    "Tue": ['국어(성)', '컴그', '컴그', '사회', '과학', 'CA', 'CA'],
+    "Wed": ['과학', '국어(성)', '체육', '상경', '컴그', '컴그', ''],
+    "Thu": ['사회', '상경', '영어(공)', '역사', '수학(류)', '미술', 'HR'],
+    "Fri": ['수학(류)', '과학', '국어(김)', '진로', '영어(공)', '체육', '역사'],
+    "Sat": ['No Schedule'],
+    "Sun": ['No Schedule']
+  },
+  "1-3": {
+    "Mon": ['영어(공)', '자료구조 A', '자료구조 A', '음악', '자료구조 B', '자료구조 B', '수학(류)'],
+    "Tue": ['수학(류)', '과학', '체육', '음악', '컴일', '진로', 'CA'],
+    "Wed": ['자료구조 A', '영어(공)', '진로', '국어(성)', '사회', '수학(류)'],
+    "Thu": ['컴일', '과학', '사회', '국어(성)', '음악', '체육', 'HR'],
+    "Fri": ['국어(성)', '사회', '자료구조 B', '컴일', '자료구조 A', '과학', '영어(공)'],
+    "Sat": ['No Schedule'],
+    "Sun": ['No Schedule']
+  },
+  "1-4": {
+    "Mon": ['진로', '자료구조 B', '음악', '국어(성)', '사회', '수학(류)', '과학'],
+    "Tue": ['자료구조 B', '자료구조 B', '영어(공)', '컴일', '국어(성)', 'CA', 'CA'],
+    "Wed": ['수학(류)', '체육', '자료구조 A', '사회', '영어(공)', '과학', ''],
+    "Thu": ['음악', '국어(성)', '과학', '컴일', '자료구조 A', '자료구조 A', 'HR'],
+    "Fri": ['사회', '컴일', '음악', '영어(공)', '체육', '수학(류)', '자료구조 A'],
     "Sat": ['No Schedule'],
     "Sun": ['No Schedule']
   },
   "1-5": {
-    "Mon": ['자료구조 B', '과학', '사회', '수학 - 이효현', '자료구조 A', '체육', '국어'],
-    "Tue": ['컴일', '미술', '미술', '영어 - 공정도', '수학 - 이효현', 'CA', 'CA'],
-    "Wed": ['사회', '컴일', '과학', '미술', '체육', '국어', ''],
-    "Thu": ['진로', '자료구조 A', '자료구조 B', '자료구조 B', '과학', '영어 - 공정도', 'HR'],
-    "Fri": ['자료구조 A', '자료구조 A', '수학 - 이효현', '사회', '컴일', '국어 - 김태철', '영어 - 전유원'],
+    "Mon": ['자료구조 B', '과학', '사회', '수학(이)', '자료구조 A', '체육', '국어(성)'],
+    "Tue": ['컴일', '미술', '미술', '영어(공)', '수학(이)', 'CA', 'CA'],
+    "Wed": ['사회', '컴일', '과학', '미술', '체육', '국어(성)', ''],
+    "Thu": ['진로', '자료구조 A', '자료구조 B', '자료구조 B', '과학', '영어(공)', 'HR'],
+    "Fri": ['자료구조 A', '자료구조 A', '수학(이)', '사회', '컴일', '국어(김)', '영어(전)'],
     "Sat": ['No Schedule'],
     "Sun": ['No Schedule']
   },
   "1-6": {
-    "Mon": ['수학', '미술', '국어', '과학', '체육', '영어 - 공정도', '자료구조 A'],
-    "Tue": ['사회', '자료구조 A', '자료구조 A', '국어', '진로', 'CA', 'CA'],
+    "Mon": ['수학', '미술', '국어(성)', '과학', '체육', '영어(공)', '자료구조 A'],
+    "Tue": ['사회', '자료구조 A', '자료구조 A', '국어(성)', '진로', 'CA', 'CA'],
     "Wed": ['자료구조 B', '자료구조 B', '사회', '자료구조 A', '컴일', '체육', ''],
-    "Thu": ['영어 - 공정도', '컴일', '미술', '미술', '수학 - 이효현', '과학', 'HR'],
-    "Fri": ['과학', '수학 - 이효현', '국어', '자료구조 B', '사회', '영어 - 전유원', '컴일'],
+    "Thu": ['영어(공)', '컴일', '미술', '미술', '수학(이)', '과학', 'HR'],
+    "Fri": ['과학', '수학(이)', '국어(성)', '자료구조 B', '사회', '영어(전)', '컴일'],
     "Sat": ['No Schedule'],
     "Sun": ['No Schedule']
   }
@@ -92,7 +92,7 @@ const initialize = () => {
   }
   className = localStorage.getItem('class');
   let elements = document.getElementsByTagName('option');
-  if (elements[0].value == '') {
+  if (elements[0].value == '선택해주세요') {
     classSelector.remove(0);
   }
   for (let i = 0; i < elements.length; i++) {
@@ -112,11 +112,11 @@ const loop = () => {
     links["CA"] = links["자료구조 A"];
     links["HR"] = links["자료구조 A"];
   } else if (className == "1-1") {
-    links["CA"] = links["영어 - 공정도"];
-    links["HR"] = links["영어 - 공정도"];
+    links["CA"] = links["영어(공)"];
+    links["HR"] = links["영어(공)"];
   } else if (className == "1-2") {
-    links["CA"] = links["수학 - 류명희"];
-    links["HR"] = links["수학 - 류명희"];
+    links["CA"] = links["수학(류)"];
+    links["HR"] = links["수학(류)"];
   } else if (className == "1-5") {
     links["CA"] = links["음악"];
     links["HR"] = links["음악"];
