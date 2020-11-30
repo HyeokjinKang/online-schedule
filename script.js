1795c930b997c8399486553fba43b0759b19353d
HyeokjinKang <coupy@croissant.tech>
2020-11-30 15:54:18 +0000

    패치 수정

--- script.js
+++ script.js
@@ -167,7 +167,7 @@ const loop = () => {
     return;
   }
   if(d.getDate() == 31) {
-    todaySchedule = schedule[className].Tue;
+    todaySchedule = schedule[className].Thu;
   }
   let elements = document.getElementsByClassName('schedule');
   for (let i = 0; i < 7; i++) {
